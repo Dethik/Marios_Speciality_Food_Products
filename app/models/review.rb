ffbf4f404fab375b6b0b19c14691bf9c7bc0307b
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-03-19 16:16:40 -0700

    pass all spec tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :product
-  
+
   validates :author, presence: true
   validates :content_body, :presence => true
-  validates_length_of :content_body, :minimum => 50, :maximum => 250
+  validates_length_of :content_body, :minimum => 10, :maximum => 250
   validates :rating, :presence => true
   validates :rating,  numericality: { only_integer: true, greater_than: 0, less_than: 6 }
 
