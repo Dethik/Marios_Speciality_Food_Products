1c9db70f4ff8ef18c8721cef1caeef957fd00f46
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-03-19 15:58:16 -0700

    add spec files and pass first four tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,10 +16,3 @@ end
 10.times do |index|
   Product.create!(name: Faker::Food.unique.fruits, country: Faker::Address.country, price: Faker::Number.decimal(2).to_i)
 end
-
-Product.all.each do |product|
-  5.times do |index|
-    char_num = rand(50..250)
-    @product.reviews.create(author: Faker::Name.name, content_body: Faker::Lorem.paragraph_by_chars(char_num, false), rating: Faker::Number.between(1, 5), product_id: product.id)
-  end
-end
