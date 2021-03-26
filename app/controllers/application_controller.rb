2927750c2ace86a07c6d583d5ac03dfa491bba67
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-03-26 13:13:15 -0700

    migrate db and start on navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user
+  helper_method :user_session
+  helper_method :user_signed_in?
+
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:avatar, :email, :password, :password_confirmation, :current_password) }
+  end
 end
