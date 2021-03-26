e3228c01451e43369ab8473cdaa5adfc1eba9bdc
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-03-25 23:20:44 -0700

    add landing page, and fix specs for reviews & products

---
+++ spec/features/add_review_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "the add a review process" do
+  it "makes a review" do
+    product = Product.create(:name => 'Hamburger', :country => 'Hamburg', :price => 5.99)
+    visit product_path(product.id)
+    click_on 'Add Review'
+    fill_in 'Author', :with => 'Dinkleburg'
+    fill_in 'Content body', :with => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam molestie vel orci eu fermentum. Morbi mi metus, luctus ac sapien vitae, accumsan vehicula tortor.'
+    fill_in 'Rating', :with => 5
+    click_on 'Create Review'
+    expect(page).to have_content 'Dinkleburg'
+  end
+end
